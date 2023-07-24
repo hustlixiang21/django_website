a0f5b4fd434adbadd2e474432f745e33e2afb04a
lixiang <1194635281@qq.com>
2023-07-24 17:11:00 +0800

    create login, register and logout system

--- static/js/src/menu/zbase.js
+++ static/js/src/menu/zbase.js
@@ -18,6 +18,7 @@ class AcGameMenu {
     </div>
 </div>
 `);
+        this.$menu.hide();
         this.root.$ac_game.append(this.$menu);
         this.$single_mode = this.$menu.find('.ac-game-menu-field-item-single-mode');
         this.$multi_mode = this.$menu.find('.ac-game-menu-field-item-multi-mode');
@@ -33,6 +34,7 @@ class AcGameMenu {
     add_listening_events() {
         let outer = this;
         this.$single_mode.click(function() {
+            console.log("Single-mode!");
             outer.hide();
             outer.root.playground.show();
         });
@@ -40,7 +42,8 @@ class AcGameMenu {
             console.log("Multi-mode!");
         });
         this.$settings.click(function() {
-            console.log("Settings");
+            console.log("click settings");
+            outer.root.settings.logout_on_remote();
         });
     }
 
